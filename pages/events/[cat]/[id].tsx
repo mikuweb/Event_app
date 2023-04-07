55113c0e9c0423724f76d5263709ff9ad9eb4c0f
mikuweb <mxxx.kwtmg@gmail.com>
2023-04-07 10:56:31 +0900

    implement each event page

--- pages/events/[cat]/[id].tsx
+++ pages/events/[cat]/[id].tsx
@@ -1,5 +1,52 @@
-const EventPage = () => {
-  return <h1>Our Single Event</h1>;
+import { NextPage } from "next";
+import Image from "next/image";
+
+interface Event {
+  id: string;
+  title: string;
+  city: string;
+  description: string;
+  image: string;
+  emails_registered: string[];
+}
+
+interface EventPageProps {
+  data: Event;
+}
+
+const EventPage: NextPage<EventPageProps> = ({ data }) => {
+  return (
+    <div>
+      <Image src={data.image} width={1000} height={500  } alt={data.title} />
+      <h1>{data.title}</h1>
+      <p>{data.description}</p>
+    </div>
+  );
 };
 
 export default EventPage;
+
+export async function getStaticPaths() {
+  const { allEvents } = await import("../../../data/data.json");
+  const allPaths = allEvents.map((path: Event) => {
+    return {
+      params: {
+        cat: path.city,
+        id: path.id,
+      },
+    };
+  });
+  return {
+    paths: allPaths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const id = context.params.id;
+  const { allEvents } = await import("../../../data/data.json");
+  const eventData = allEvents.find((ev) => id === ev.id);
+  return {
+    props: { data: eventData },
+  };
+}
